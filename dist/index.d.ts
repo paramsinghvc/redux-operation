8950d1af192e01bc952215a075c798be5997110f
Parminder Singh <parminder.singh1@swiggy.in>
2019-09-28 08:05:23 +0530

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import { IAction, IActionFactory } from "./types";
 export * from "./augmentReducer";
 export * from "./types";
+export * from "./makeConstants";
 export declare const createActionWithPayload: <T extends string | symbol, P>(type: T) => (payload?: P) => IAction<T, P>;
 export interface IReduxOperations<T = any> {
     pending: boolean;
@@ -16,7 +17,7 @@ export declare enum actionFlags {
     CLEAR = "CLEAR"
 }
 export declare const createReduxOperation: (actionName: string) => {
-    constants: import("./makeConstants").IConstants;
+    constants: import("./types").IConstants;
     actions: IActionFactory<symbol, unknown>[];
     reducer: (state: IReduxOperations<any>, { type, payload }: {
         type: any;
